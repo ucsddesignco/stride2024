57e3fefe40891fde659c6c40db84e203cf26b4e9
Sahil Gathe <sahilsankur@gmail.com>
2024-10-27 18:45:04 -0700

    feat: sloppy flashlight implmentation

---
+++ src/app/components/flashlight/flashlight.tsx
@@ -0,0 +1,10 @@
+// import React from "react";
+// import './flashlight.scss';
+// import Cursor from 'public/Cursor.svg';
+
+// const Flashlight: React.FC = () => {
+//     return (
+//         <div className="flashlight">
+//         </div>
+//     );
+// };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+import { useEffect } from 'react';
+import React from 'react';
+import './styles/flashlight.scss';
+
 export default function Home() {
+  useEffect(() => {
+    const flashlight = document.querySelector('.custom-cursor');
+
+    const moveCursor = (e: MouseEvent) => {
+      document.documentElement.style.setProperty('--cursor-x', `${e.clientX}px`);
+      document.documentElement.style.setProperty('--cursor-y', `${e.clientY}px`);
+    };
+
+    window.addEventListener('mousemove', moveCursor);
+
+    return () => {
+      window.removeEventListener('mousemove', moveCursor);
+    };
+  }, []);
+
   return (
-    <main>
+    <main className='flashlight'>
       <h1>Stride 2024</h1>
+      {/* temp to test flashlight cursor*/}
+      <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+        <img src="/Stridebackground.png" alt="stride image" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      </div>
+      <div className="custom-cursor"></div>
     </main>
   );
 }
