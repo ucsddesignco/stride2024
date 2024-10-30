1cd20fef1d90a46da0de80c101c731c86a8189be
Sahil Gathe <sahilsankur@gmail.com>
2024-10-29 17:51:38 -0700

    Update: added ringsand made it compnent

--- src/app/components/flashlight/flashlight.tsx
+++ src/app/components/flashlight/flashlight.tsx
@@ -1,10 +1,30 @@
-// import React from "react";
-// import './flashlight.scss';
-// import Cursor from 'public/Cursor.svg';
+import React from "react";
+import { useEffect } from 'react';
+import './flashlight.scss';
+import Cursor from 'public/Cursor.svg';
 
-// const Flashlight: React.FC = () => {
-//     return (
-//         <div className="flashlight">
-//         </div>
-//     );
-// };
+const Flashlight: React.FC = () => {
+    useEffect(() => {
+
+        const moveCursor = (e: MouseEvent) => {
+          document.documentElement.style.setProperty('--cursor-x', `${e.clientX}px`);
+          document.documentElement.style.setProperty('--cursor-y', `${e.clientY}px`);
+        };
+    
+        window.addEventListener('mousemove', moveCursor);
+    
+        return () => {
+          window.removeEventListener('mousemove', moveCursor);
+        };
+      }, []);
+
+    return (
+        <>
+            <div className="custom-cursor"></div>
+            <div className="ring-inner"></div>
+            <div className="ring-outer"></div>
+        </>
+    );
+};
+
+export default Flashlight;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,17 @@
 "use client";
-import { useEffect } from 'react';
 import React from 'react';
-import './styles/flashlight.scss';
-
+import Flashlight from './components/flashlight/flashlight';
 export default function Home() {
-  useEffect(() => {
-    const flashlight = document.querySelector('.custom-cursor');
-
-    const moveCursor = (e: MouseEvent) => {
-      document.documentElement.style.setProperty('--cursor-x', `${e.clientX}px`);
-      document.documentElement.style.setProperty('--cursor-y', `${e.clientY}px`);
-    };
-
-    window.addEventListener('mousemove', moveCursor);
-
-    return () => {
-      window.removeEventListener('mousemove', moveCursor);
-    };
-  }, []);
 
   return (
     <main className='flashlight'>
       <h1>Stride 2024</h1>
       {/* temp to test flashlight cursor*/}
       <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
-        <img src="/Stridebackground.png" alt="stride image" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+        <img src="/Homepage_Desktop.png" alt="stride image" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
       </div>
-      <div className="custom-cursor"></div>
+      <p>This is a test P</p>
+      <Flashlight />
     </main>
   );
 }
