d0670ecd77667c0bdbea8491c5244f6ced5f5c80
EdwardNew <pkarrows@gmail.com>
2024-11-02 01:59:53 -0700

    feat: added duplicate images and animated inital folder open

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,129 +1,163 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { gsap } from 'gsap';
+import { useState, useRef, useEffect } from 'react';
+import { gsap, Power2, Power3, Power4 } from 'gsap';
 import { useGSAP } from '@gsap/react';
 import '@/app/home.scss';
 import Image from 'next/image';
-import Flashlight from '@/components/Flashlight/Flashlight';
+// import Flashlight from '@/components/Flashlight/Flashlight';
 
 gsap.registerPlugin(useGSAP);
 
 export default function Page() {
+  const [folderOpenned, setFolderOpenned] = useState(false);
   const [isHoveringFolder, setIsHoveringFolder] = useState(false);
 
-  useEffect(() => {}, []);
+  const badge = useRef<HTMLImageElement>(null);
+  const feather = useRef<HTMLImageElement>(null);
+  const mainContent = useRef<HTMLImageElement>(null);
+  const polaroid = useRef<HTMLImageElement>(null);
+  const orgs = useRef<HTMLImageElement>(null);
+  const companies = useRef<HTMLImageElement>(null);
+  const magnifyingGlass = useRef<HTMLImageElement>(null);
+  const cards = useRef<HTMLImageElement>(null);
+  const pageFolderBottom = useRef<HTMLImageElement>(null);
+
+  const folderBadge = useRef<HTMLImageElement>(null);
+  const folderFeather = useRef<HTMLImageElement>(null);
+  const folderMainContent = useRef<HTMLImageElement>(null);
+  const folderPolaroid = useRef<HTMLImageElement>(null);
+  const folderOrgs = useRef<HTMLImageElement>(null);
+  const folderCompanies = useRef<HTMLImageElement>(null);
+  const folderMagnifyingGlass = useRef<HTMLImageElement>(null);
+  const folderCards = useRef<HTMLImageElement>(null);
+  const folderBottom = useRef<HTMLImageElement>(null);
+
+  //   const setPageAssetPosition = (
+  //     pageAssetRef: RefObject<HTMLImageElement>,
+  //     folderAssetRef: RefObject<HTMLImageElement>
+  //   ): void => {
+  //     const folderAssetRect = folderAssetRef.current?.getBoundingClientRect();
+  //     const pageAsset = pageAssetRef.current;
+  //     if (folderAssetRect && pageAsset) {
+  //       pageAsset.style.top = `${folderAssetRect.top}px`;
+  //       pageAsset.style.left = `${folderAssetRect.left}px`;
+  //     }
+  //   };
+
+  useEffect(() => {
+    const setNonFolderPositions = () => {
+      const folderBadgeRect = folderBadge.current?.getBoundingClientRect();
+      const folderFeatherRect = folderFeather.current?.getBoundingClientRect();
+      const folderMainContentRect =
+        folderMainContent.current?.getBoundingClientRect();
+      const folderPolaroidRect =
+        folderPolaroid.current?.getBoundingClientRect();
+      const folderOrgsRect = folderOrgs.current?.getBoundingClientRect();
+      const folderCompaniesRect =
+        folderCompanies.current?.getBoundingClientRect();
+      const folderMagnifyingGlassRect =
+        folderMagnifyingGlass.current?.getBoundingClientRect();
+      const folderCardsRect = folderCards.current?.getBoundingClientRect();
+      const folderBottomRect = folderBottom.current?.getBoundingClientRect();
+
+      if (folderBadgeRect && badge.current) {
+        badge.current.style.position = 'absolute';
+        badge.current.style.top = `${folderBadgeRect.top}px`;
+        badge.current.style.left = `${folderBadgeRect.left}px`;
+      }
+      if (folderFeatherRect && feather.current) {
+        feather.current.style.position = 'absolute';
+        feather.current.style.top = `${folderFeatherRect.top}px`;
+        feather.current.style.left = `${folderFeatherRect.left}px`;
+      }
+      if (folderMainContentRect && mainContent.current) {
+        mainContent.current.style.position = 'absolute';
+        mainContent.current.style.top = `${folderMainContentRect.top}px`;
+        mainContent.current.style.left = `${folderMainContentRect.left}px`;
+      }
+      if (folderPolaroidRect && polaroid.current) {
+        polaroid.current.style.position = 'absolute';
+        polaroid.current.style.top = `${folderPolaroidRect.top}px`;
+        polaroid.current.style.left = `${folderPolaroidRect.left}px`;
+      }
+      if (folderOrgsRect && orgs.current) {
+        orgs.current.style.position = 'absolute';
+        orgs.current.style.top = `${folderOrgsRect.top}px`;
+        orgs.current.style.left = `${folderOrgsRect.left}px`;
+      }
+      if (folderCompaniesRect && companies.current) {
+        companies.current.style.position = 'absolute';
+        companies.current.style.top = `${folderCompaniesRect.top}px`;
+        companies.current.style.left = `${folderCompaniesRect.left}px`;
+      }
+      if (folderMagnifyingGlassRect && magnifyingGlass.current) {
+        magnifyingGlass.current.style.position = 'absolute';
+        magnifyingGlass.current.style.top = `${folderMagnifyingGlassRect.top}px`;
+        magnifyingGlass.current.style.left = `${folderMagnifyingGlassRect.left}px`;
+      }
+      if (folderCardsRect && cards.current) {
+        cards.current.style.position = 'absolute';
+        cards.current.style.top = `${folderCardsRect.top}px`;
+        cards.current.style.left = `${folderCardsRect.left}px`;
+      }
+      if (folderBottomRect && pageFolderBottom.current) {
+        pageFolderBottom.current.style.position = 'absolute';
+        pageFolderBottom.current.style.top = `${folderBottomRect.top}px`;
+        pageFolderBottom.current.style.left = `${folderBottomRect.left}px`;
+      }
+    };
+
+    setNonFolderPositions();
+
+    // Optionally, update positions on window resize
+    // window.addEventListener('resize', setNonFolderPositions);
+    // return () => window.removeEventListener('resize', setNonFolderPositions);
+  }, []);
 
   const openFolder = () => {
+    setFolderOpenned(true);
     const tl = gsap.timeline();
-    tl.to('#folder-flap', { rotateX: 120 });
-    tl.to('#click-to-open-text', { display: 'none', duration: 0 });
-    tl.to('#folder', {
-      translateY: 700,
+    tl.to('#folder-flap', {
+      rotateX: 160,
+      duration: 0.5,
+      ease: Power2.easeInOut
+    }).add(() => {
+      gsap.set('.folder-asset', { display: 'none' });
+      gsap.set('#click-to-open-text', { visibility: 'hidden' });
+      gsap.set('.page-asset', { display: 'block' });
+    });
+    tl.to(['#folder', '#page-folder-bottom'], {
+      translateY: '105vh',
       translateX: -50,
       rotate: 10,
-      duration: 1
+      duration: 1.5,
+      ease: Power4.easeOut
     });
-    tl.to(
-      '.folder-asset',
-      {
-        scale: 1.2
-      },
-      '<'
-    );
-    tl.to(
-      '#badge',
-      {
-        right: 0,
-        top: '60%',
-        rotate: 0,
-        duration: 1
-      },
-      '<'
-    );
-    tl.to(
-      '#feather',
-      {
-        left: '45%',
-        top: '0%',
-        rotate: -15,
-        duration: 1
-      },
-      '<'
-    );
-    tl.to(
-      '#polaroid',
-      {
-        left: '40%',
-        top: '-5%',
-        duration: 1
-      },
-      '<'
-    );
-    tl.to(
-      '#orgs',
-      {
-        left: '0%',
-        top: '68%',
-        rotate: 0,
-        duration: 1
-      },
-      '<'
-    );
-    tl.to(
-      '#companies',
-      {
-        right: '0%',
-        top: '20%',
-        rotate: 0,
-        duration: 1
-      },
-      '<'
-    );
-    tl.to(
-      '#cards',
-      {
-        left: '5%',
-        top: '25%',
-        rotate: 8,
-        duration: 1
-      },
-      '<'
-    );
-    tl.to(
-      '#magnifying-glass',
-      {
-        left: '-2%',
-        top: '15%',
-        rotate: 53,
-        duration: 1
-      },
-      '<'
-    );
-    tl.to(
-      '#main-content',
-      {
-        top: '50%',
-        duration: 1
-      },
-      '<'
-    );
   };
 
   useGSAP(
     () => {
+      if (folderOpenned) {
+        return;
+      }
+
       if (isHoveringFolder) {
-        gsap.to('#folder-flap', { rotateX: '30' });
+        gsap.to('#folder-flap', {
+          rotateX: '30',
+          duration: 0.5,
+          ease: Power3.easeOut
+        });
       } else {
         gsap.to('#folder-flap', { rotateX: '0' });
       }
     },
-    { dependencies: [isHoveringFolder] }
+    { dependencies: [isHoveringFolder, folderOpenned] }
   );
 
   return (
     <div style={{ minHeight: '100vh', height: '100vh' }}>
-      <Flashlight />
+      {/* <Flashlight /> */}
       <div
         style={{
           backgroundColor: 'black',
@@ -134,8 +168,7 @@ export default function Page() {
           gap: '3rem',
           height: '100%',
           overflow: 'hidden',
-          position: 'relative',
-          perspective: 4000
+          position: 'relative'
         }}
       >
         <Image
@@ -168,26 +201,29 @@ export default function Page() {
         />
 
         <Image
-          src="/images/page-assets/cards.svg"
-          className="folder-asset"
-          alt=""
           id="cards"
+          ref={cards}
+          className="page-asset"
+          src="/images/page-assets/cards.svg"
+          alt=""
           width={263}
           height={188}
         />
 
         <Image
+          id="magnifying-glass"
+          ref={magnifyingGlass}
+          className="page-asset"
           src="/images/page-assets/magnifying-glass.svg"
           alt=""
-          id="magnifying-glass"
-          className="folder-asset"
           width={307}
           height={513}
         />
 
         <Image
           id="companies"
-          className="folder-asset"
+          ref={companies}
+          className="page-asset"
           src="/images/page-assets/companies.svg"
           alt=""
           width={264}
@@ -196,7 +232,8 @@ export default function Page() {
 
         <Image
           id="orgs"
-          className="folder-asset"
+          ref={orgs}
+          className="page-asset"
           src="/images/page-assets/orgs.svg"
           alt=""
           width={259}
@@ -205,7 +242,8 @@ export default function Page() {
 
         <Image
           id="polaroid"
-          className="folder-asset"
+          ref={polaroid}
+          className="page-asset"
           src="/images/page-assets/polaroid.svg"
           alt=""
           width={132}
@@ -214,7 +252,8 @@ export default function Page() {
 
         <Image
           id="main-content"
-          className="folder-asset"
+          ref={mainContent}
+          className="page-asset"
           src="/images/page-assets/main-content.svg"
           alt=""
           width={406}
@@ -223,7 +262,8 @@ export default function Page() {
 
         <Image
           id="feather"
-          className="folder-asset"
+          ref={feather}
+          className="page-asset"
           src="/images/page-assets/feather.svg"
           alt=""
           width={266}
@@ -232,11 +272,22 @@ export default function Page() {
 
         <Image
           id="badge"
-          className="folder-asset"
+          ref={badge}
+          className="page-asset"
           src="/images/page-assets/badge.svg"
           alt=""
           width={194}
           height={260}
+        />
+
+        <Image
+          id="page-folder-bottom"
+          ref={pageFolderBottom}
+          className="page-asset"
+          src="/images/folder/flat-folder-bottom.svg"
+          alt=""
+          width={754}
+          height={365}
         />
 
         <div
@@ -261,8 +312,83 @@ export default function Page() {
             width={754}
             height={535}
           />
+
+          <Image
+            id="folder-cards"
+            ref={folderCards}
+            className="folder-asset"
+            src="/images/page-assets/cards.svg"
+            alt=""
+            width={263}
+            height={188}
+          />
+          <Image
+            id="folder-magnifying-glass"
+            ref={folderMagnifyingGlass}
+            className="folder-asset"
+            src="/images/page-assets/magnifying-glass.svg"
+            alt=""
+            width={307}
+            height={513}
+          />
+          <Image
+            id="folder-companies"
+            ref={folderCompanies}
+            className="folder-asset"
+            src="/images/page-assets/companies.svg"
+            alt=""
+            width={264}
+            height={179}
+          />
+          <Image
+            id="folder-orgs"
+            ref={folderOrgs}
+            className="folder-asset"
+            src="/images/page-assets/orgs.svg"
+            alt=""
+            width={259}
+            height={208}
+          />
+          <Image
+            id="folder-polaroid"
+            ref={folderPolaroid}
+            className="folder-asset"
+            src="/images/page-assets/polaroid.svg"
+            alt=""
+            width={132}
+            height={150}
+          />
+          <Image
+            id="folder-main-content"
+            ref={folderMainContent}
+            className="folder-asset"
+            src="/images/page-assets/main-content.svg"
+            alt=""
+            width={406}
+            height={208}
+          />
+          <Image
+            id="folder-feather"
+            ref={folderFeather}
+            className="folder-asset"
+            src="/images/page-assets/feather.svg"
+            alt=""
+            width={266}
+            height={150}
+          />
+          <Image
+            id="folder-badge"
+            ref={folderBadge}
+            className="folder-asset"
+            src="/images/page-assets/badge.svg"
+            alt=""
+            width={194}
+            height={260}
+          />
+
           <Image
             id="folder-bottom"
+            ref={folderBottom}
             src="/images/folder/flat-folder-bottom.svg"
             alt=""
             width={754}
